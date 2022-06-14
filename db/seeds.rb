b24ea6461816f04d0b185c53b372e43af91a4c37
Gegardus <stgegard@gmail.com>
2022-06-14 11:37:33 +0400

    Create db, table and an API endpoint that selects a greeting table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(:message => "Hello!  Welcome to Rails React App!")
+Greeting.create(:message => "Hi! This is a preliminary draft before the final capstone.")
+Greeting.create(:message => "Greetings! Greetings! Greetings!")
+Greeting.create(:message => "Hey, developer, keep it up!")
+Greeting.create(:message => "We welcoming everyone who enters web-develpment world!")
+
+puts 'Seed files added successfully!'
